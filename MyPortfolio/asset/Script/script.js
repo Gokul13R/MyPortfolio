2c9758c4897a357060312f948b4ddbd753400740
Gokul <gokulrajendran037@gmail.com>
2025-10-30 09:41:29 +0530

    gg

--- MyPortfolio/asset/Script/script.js
+++
@@ -1,7 +0,0 @@
-function showProject(projectId) {
-  const projects = document.querySelectorAll('.project-content');
-  projects.forEach(project => project.classList.remove('active'));
-
-  const selected = document.getElementById(projectId);
-  selected.classList.add('active');
-}
