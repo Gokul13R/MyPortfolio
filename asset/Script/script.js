5a63b1426cfc7a147d1cf776ef594df0c2210b38
Gokul <gokulrajendran037@gmail.com>
2025-10-30 15:28:53 +0530

    gg

--- asset/Script/script.js
+++ asset/Script/script.js
@@ -1,3 +1,11 @@
+let menuIcon = document.querySelector('.menu-icon')
+let navber = document.querySelector('.navbar')
+
+menuIcon.onclick=()=>{
+  navber.classList.toggle('active');
+}
+
+
 function showProject(projectId) {
   const projects = document.querySelectorAll('.project-content');
   projects.forEach(project => project.classList.remove('active'));
